1e6935c1a8ddd1ab29f942c05cb072b0bf721bd6
Sean Doyle <sdoylelambda@gmail.com>
2019-03-04 19:01:45 -0500

    Nav completed.

--- js/index.js
+++ js/index.js
@@ -45,29 +45,122 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 let pic = document.getElementById("cta-img");
 pic.setAttribute('src', siteContent["cta"]["img-src"]);
 
-// 3rd pic
-let lastPic = document.getElementById("middle-img");
-lastPic.setAttribute('srs', siteContent["middle-img"]["img-src"]);
+// // 3rd pic
+// let lastPic = document.getElementById("middle-img");
+// lastPic.setAttribute('src', siteContent["middle-img"]["img-src"]);
 
-// Nav 
-// let nav = document.getElementsByTagName("nav");
-// nav.setAttribute('src', siteContent["nav"]["nav-item-1"]);
-// let nav = document.getElementsById("nav");
-// nav.setAttribute('src', siteContent["nav"]["nav-item-1"]);
+// // Nav 
+// // let nav = document.getElementsByTagName("nav");
+// // nav.setAttribute('src', siteContent["nav"]["nav-item-1"]);
+// // let nav = document.getElementsById("nav");
+// // nav.setAttribute('src', siteContent["nav"]["nav-item-1"]);
+const navigation = document.querySelector('nav');
+navigation.children[0].textContent = 'Services';
+navigation.children[1].textContent = 'Services';
+navigation.children[2].textContent = 'Services';
+navigation.children[3].textContent = 'Services';
+navigation.children[4].textContent = 'Services';
+navigation.children[5].textContent = 'Services';
 
-// a tag
-let a = document.querySelector('a');
-a.setAttribute('src', siteContent["nav"]["img-src"]);
+// // a tag
+// let a = document.querySelector('a');
+// a.setAttribute('src', siteContent["nav"]["img-src"]);
 
-// button
-const btnID = document.getElementById('button');
-btnID.textContent = "clinkk"
+// // button
+let btnID = document.getElementById("button");
+//btnID.textContent = ("Get Started");
 
-// Main Paragraph
-const ftures = document.createElement('p');
-ftures.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// // Main Paragraph
+// const ftures = document.createElement('p');
+// ftures.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+// // const secContent = document.querySelector('.contact');
+// // secContent.appendChild(ftures);
 
 
 
-// const secContent = document.querySelector('.contact');
-// secContent.appendChild(ftures);
+
+// const newNavItems = ['Boards', 'Bikes'] 
+// newNavItems.forEach(item => {
+//   const anchorItem = document.createElement('a')
+//   anchorItem.text = item
+//   anchorItem.href = '#'
+//   anchorItem.style.color = 'green'
+//   document.querySelector('nav').appendChild(anchorItem)
+// })
+
+// CTA
+// document.querySelector('#cta-img').scr = siteContent['cta']['img-src']
+// document.querySelector('.cta-text button').innerHTML = siteContent['cta']['button']
+// document.querySelector(
+//   'cta-text h1'
+// ).innerHTML = siteContent['cta']['h1']
+
+// // Contact
+// const contact = document.querySelector('.contact')
+// contact.querySelector('h4').innerHTML = siteContent['contact']['contact-h4']
+// const contactPtags = contact.querySelector('p')
+// contactPtags[0].innerHTML = siteContent['contact']['address']
+// contactPtags[1].innerHTML = siteContent['contact']['phone']
+// contactPtags[2].innerHTML = siteContent['contact']['email']
+
+// // Footer
+//document.querySelector('footer p').innerHTML = 'Copyright Great Idea! 2018'
+
+
+
+
+
+// // Links
+
+// let linkData = siteContent["nav"];
+
+// // Task 3 New Content
+// let node = document.createElement("a");
+// console.log(node);
+// let testNode = document.createNewNode("Hello World");
+// console.log(textNode);
+// let text = node.appendChild(testNode);
+
+// document.querySelector("nav").appendChild(testNode);
+
+// let links = document.querySelector("a");
+// links.forEach(function(item, i) {
+//   item.style.color = "green";
+//   item.textContent = linksData['nav-item-${i + 1}'];
+// });
+
+// // CTA
+// let cta = siteContent["cta"];
+
+// // CTA IMG
+// let ctaImg = document.getElementById("cta-img");
+// ctaImg.src = cta("img-src");
+
+// // CTA Text
+// let ctaText = document.querySelector(".cta-text");
+// let ctaChil = ctaText.childNodes;
+
+// // for loop
+
+
+
+
+
+
+
+// H4
+
+
+
+
+//cta text
+let h1Text = document.querySelector("h1");
+h1Text.innerText = "DOM <br> is <br>  Awesome";
+
+// Bottom Content
+// let bottomText = Array.from(document.getElementsByClassName('text-content')
+// );
+// bottomText.forEach(function)(elem) {
+//   const elmArr = Array.from(dlem.children);
+// };
